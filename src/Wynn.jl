ca2722b50be22c1dfdf0a45b55cb940ac5d9624d
Jeremy <j.d.revell1@ncl.ac.uk>
2019-03-18 22:25:37 +0000

    fixed index bugs for ugenerating complete epsilon table

--- src/Wynn.jl
+++ src/Wynn.jl
@@ -11,19 +11,20 @@ module Wynn
 
     function EpsilonTable(terms::Vector{T}; simplified::Bool = true) where T<:Union{Real,Sym}
         # maximum occuring i & j index, minus 1 for nonzero julia indexing
-        max_ind = length(terms) - 1
+        max_ind = length(terms)
 
         # setting base case j = -1
         etable = Dict((i, -1) => eltype(terms)(0) for i in 0:max_ind)
 
-        # setting base case j = 0, i = 0:max_ind
-        merge!(etable, Dict((i, 0) => sum(terms[1:i+1]) for i in 0:max_ind))
+        # setting base case j = 0, i = 0:max_ind-1
+        merge!(etable, Dict((i-1, 0) => sum(terms[1:i]) for i in 1:max_ind))
 
         # setting base case i = -j-1, j even
-        merge!(etable, Dict((-j-1, 2j) => eltype(terms)(0) for j in 0:max_ind/2))
+        merge!(etable, Dict((-j-1, 2j) => eltype(terms)(0) for j in 0:max_ind))
+        merge!(etable, Dict((-j-1, 2j-1) => eltype(terms)(0) for j in 0:max_ind))
 
-        # recursive calculations, j>=1, i = -floor(Int,(j+1)/2):max_ind-j
-        for j in 1:max_ind, i in -floor(Int,(j+1)/2):max_ind-j
+        # recursive calculations, j>=1, i = floor(Int,-j/2):(max_ind-j-1)
+        for j in 1:2*(max_ind-1), i in floor(Int,-j/2):(max_ind-j-1)#j-1
             ϵ_ij = etable[i+1, j-2] + 1 / (etable[i+1, j-1] - etable[i, j-1])
             eltype(terms) <: Sym && simplified ? push!(etable, (i, j) =>
                 simplify(ϵ_ij)) : push!(etable, (i, j) => ϵ_ij)
